421c17a5667faf3b3a6e7d0fc5bf66eb714bffcd
LesFigue <112564946+LesFigue@users.noreply.github.com>
2022-09-20 20:41:37 -0600

    Add files via upload

---
+++ Practica1.2calherencia/Practica1.2calherencia/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Practica1._2calherencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.txtv2 = new System.Windows.Forms.TextBox();
+            this.txtv1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(388, 242);
+            this.txtresultado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.ReadOnly = true;
+            this.txtresultado.Size = new System.Drawing.Size(151, 26);
+            this.txtresultado.TabIndex = 19;
+            this.txtresultado.TextChanged += new System.EventHandler(this.txtresultado_TextChanged);
+            // 
+            // txtv2
+            // 
+            this.txtv2.Location = new System.Drawing.Point(388, 176);
+            this.txtv2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtv2.Name = "txtv2";
+            this.txtv2.Size = new System.Drawing.Size(151, 26);
+            this.txtv2.TabIndex = 18;
+            // 
+            // txtv1
+            // 
+            this.txtv1.Location = new System.Drawing.Point(388, 112);
+            this.txtv1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtv1.Name = "txtv1";
+            this.txtv1.Size = new System.Drawing.Size(151, 26);
+            this.txtv1.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 245);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Resultado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 182);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Valor 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Valor 1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(614, 396);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 37);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Dividir (/)";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(440, 396);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 37);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Multiplicar (x)";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 396);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 37);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Restar (-)";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 396);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Sumar (+)";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 506);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtv2);
+            this.Controls.Add(this.txtv1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.TextBox txtv2;
+        private System.Windows.Forms.TextBox txtv1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
